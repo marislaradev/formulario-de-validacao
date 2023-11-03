87f4f661b608ed2ecabd60570a7321a7dbd08f68
Mariana da Silva Lara <121054908+marislaradev@users.noreply.github.com>
2023-11-03 13:37:30 -0300

    chore: finalized project

---
+++ src/js/validarCampos.js
@@ -0,0 +1,47 @@
+const camposDeEntrada = document.querySelectorAll(".campo, .campo-mensagem");
+
+camposDeEntrada.forEach((campo) => {
+  campo.addEventListener("focus", () => {
+    campo.classList.remove("campo-preenchido"); 
+    campo.classList.add("campo-vazio"); 
+    const mensagemErro = campo.nextElementSibling; 
+    mensagemErro.style.display = "none";
+  });
+
+  campo.addEventListener("blur", () => {
+    if (campo.value.trim() === "") {
+      campo.classList.add("campo-vazio");
+    } else {
+      campo.classList.add("campo-preenchido");
+    }
+  });
+});
+
+const botaoEnviar = document.querySelector(".botao-enviar");
+
+botaoEnviar.addEventListener("click", (evento) => {
+  let temCampoVazio = false;
+
+  camposDeEntrada.forEach((campo) => {
+    if (campo.value.trim() === "") {
+      campo.classList.add("campo-vazio");
+
+      const mensagemErro = campo.nextElementSibling;
+      mensagemErro.style.display = "block";
+
+      temCampoVazio = true;
+    } else { 
+      campo.classList.remove("campo-vazio");
+    }
+  });
+
+  // if (temCampoVazio) {
+  //   evento.preventDefault();
+  // }
+  if (!temCampoVazio) {
+    const formulario = document.querySelector("#formulario");
+    formulario.submit();
+  } else {
+    evento.preventDefault();
+  }
+});
